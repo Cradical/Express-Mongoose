4631d86319d1622ef25bdf4e771011c15378e8bc
Cory Pease <coryrpease@gmail.com>
2018-09-25 14:32:58 -0600

    cleaned up code and comments

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const listener = () => console.log(`Listening to port ${port}!`)
 
+
 mongoose.connect(process.env.MONGODB,{ useNewUrlParser: true })
 const teamSchema = new mongoose.Schema({
     first: String,
@@ -15,42 +16,48 @@ const teamSchema = new mongoose.Schema({
 })
 const Team = mongoose.model("Team", teamSchema)
 
-// app.disable('x-powered-by')
+app.disable('x-powered-by')
 app.use(bodyParser.json())
 app.use(cors())
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-      console.error('Bad JSON');
+        console.error('Bad JSON');
     }
-  });
+});
+
+
+app.get('/ping', (req,res)=>{
+    console.log("PONG!")
+})
 
 app.get('/', (req,res) => {
-    console.log('PONG')
     Team.find({})
     .then(team => res.json({team}))
 })
 
-app.post('/', (req,res,next) =>{
+app.post('/', (req,res) =>{
     Team.create(req.body)
-    .then(team => res.status(201).json({team})).pretty()
-    next(err)
+    .then(team => res.status(201).json({team}))
 })
 
 app.put('/:id', (req,res) => {
-    console.log(req.params.id,req.body)
     Team.update({_id:req.params.id},{$set : req.body})
     .then(team => res.status(201).json({team}))
-    // next(err)
 })
 
-// Tank.update({ _id: id }, { $set: { size: 'large' }}, callback);
+app.delete('/:id', (req,res)=>{
+    Team.deleteOne({_id:req.params.id})
+    .then(team => res.status(201).json())
+})
 
 app.use((err,req,res,next)=>{
     res.status(err.status || 500).json({error:err})
 })
+
 app.use((req,res,next)=>{
     res.status(404).json({error: {message: 'Not Found!'}})
 })
+
 app.listen(port,listener)
 
 
